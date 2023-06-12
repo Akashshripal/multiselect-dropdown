100ed819e0062939f854b5fe90ae4cd94542f305
Akashshripal <98543501+Akashshripal@users.noreply.github.com>
2023-06-12 13:31:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import SelectMultidropdown from './SelectMultidropdown';
+
+function App() {
+  return (
+  <SelectMultidropdown /> 
+  ) 
+}
+
+export default App
